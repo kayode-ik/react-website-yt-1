66a8010466ea4ea8e01b4787db08455295966fb8
Kayode-ik <ipentankayode5@gmail.com>
2021-07-19 10:25:25 +0100

    Done final

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../App.css'
+import Card from '../Card/Cards'
+import Footer from '../Footer'
+import HeroSection from '../Home/HeroSection'
+
+function Home () {
+    return (
+        <>
+        <HeroSection />
+        <Card />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
